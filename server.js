496d8faa4636f34d144fee526b8a120faef9df5d
Anna <iwannabetall@gmail.com>
2021-04-02 15:31:31 -0700

    heroku deploy

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const socketIo = require("socket.io")
 const express = require('express');
 const http = require('http')
-const webpack = require('webpack');
 
 var app = express();
 
@@ -27,18 +26,6 @@ app.get('*', function(req,res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
-
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
 
 let players = [];  // player ids 
 var playerInfo = [];
